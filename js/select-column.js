4a765ad594f3845dd28570f0ebe9b7e40ac88086
SEF30 <operations@sciencesenfolie.ca>
2024-07-12 13:55:17 -0400

    Create select-column.js

---
+++ js/select-column.js
@@ -0,0 +1,26 @@
+var t = TrelloPowerUp.iframe();
+
+function fetchColumns() {
+    return t.lists('all').then(function(lists) {
+        var select = document.getElementById('column-select');
+        lists.forEach(function(list) {
+            var option = document.createElement('option');
+            option.value = list.id;
+            option.text = list.name;
+            select.add(option);
+        });
+    });
+}
+
+document.getElementById('start-focus-btn').addEventListener('click', function() {
+    var listId = document.getElementById('column-select').value;
+    t.set('board', 'shared', 'focusListId', listId).then(function() {
+        return t.closePopup();
+    }).then(function() {
+        window.open('./focus-mode.html?listId=' + listId, '_self');
+    });
+});
+
+t.render(function() {
+    fetchColumns();
+});
