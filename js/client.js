8d8facca3868a73adbdc7212d9578b8c858a1ccf
SEF30 <operations@sciencesenfolie.ca>
2024-07-12 11:15:26 -0400

    Create client.js

---
+++ js/client.js
@@ -0,0 +1,40 @@
+var GRAY_ICON = 'https://yourdomain.com/icon-gray.svg';  // Replace with your own hosted icon URL
+
+TrelloPowerUp.initialize({
+    'card-buttons': function(t, options) {
+        return [{
+            icon: GRAY_ICON,
+            text: 'Focus Mode',
+            callback: function(t) {
+                return t.popup({
+                    title: "Focus Mode",
+                    url: './focus-mode.html',
+                    height: 300
+                });
+            }
+        }];
+    },
+    'board-buttons': function(t, options) {
+        return [{
+            icon: GRAY_ICON,
+            text: 'Productivity Report',
+            callback: function(t) {
+                return t.modal({
+                    title: 'Productivity Report',
+                    url: './productivity-report.html',
+                    height: 500,
+                    fullscreen: false
+                });
+            }
+        }];
+    },
+    'card-badges': function(t, options) {
+        return t.get('card', 'shared', 'focusTime').then(function(focusTime) {
+            return [{
+                icon: GRAY_ICON,
+                text: focusTime ? focusTime + ' min focused' : 'Not started',
+                color: focusTime ? 'green' : 'red'
+            }];
+        });
+    }
+});
