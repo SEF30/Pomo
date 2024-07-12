268e1898f979ac76d9890fca489fe1d102d79bf0
SEF30 <operations@sciencesenfolie.ca>
2024-07-12 10:33:54 -0400

    app.js

--- app.js
+++ app.js
@@ -3,31 +3,41 @@ console.log('Power-Up is loading');
 var GRAY_ICON = 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-gray.svg';
 
 window.TrelloPowerUp.initialize({
-  'card-buttons': function(t, options) {
+  'list-actions': function(t) {
     return [{
-      icon: GRAY_ICON,
-      text: 'Focus Mode',
+      text: 'Start Focus Session',
       callback: function(t) {
-        return t.popup({
-          title: "Focus Mode",
+        return t.modal({
           url: './focus.html',
-          height: 300
+          height: 300,
+          title: 'Focus Session'
         });
       }
     }];
   },
-  'card-badges': function(t, options) {
-    return t.get('card', 'shared', 'focusTime')
-    .then(function(focusTime) {
-      if(focusTime) {
-        return [{
-          icon: GRAY_ICON,
-          text: focusTime + ' min focused',
-          color: 'green'
-        }];
-      } else {
-        return [];
+  'card-badges': function(t) {
+    return t.card('all')
+      .then(function(card) {
+        if (card.idList === t.getContext().list) {
+          return [{
+            icon: GRAY_ICON,
+            text: 'In Focus',
+            color: 'green'
+          }];
+        }
+      });
+  },
+  'board-buttons': function(t) {
+    return [{
+      icon: GRAY_ICON,
+      text: 'Focus Logs',
+      callback: function(t) {
+        return t.modal({
+          url: './logs.html',
+          height: 500,
+          title: 'Focus Session Logs'
+        });
       }
-    });
+    }];
   }
 });
