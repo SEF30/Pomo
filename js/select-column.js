a3f5177d85c7d61596a001f8abaadc1c58cd44cb
SEF30 <operations@sciencesenfolie.ca>
2024-07-12 14:13:46 -0400

    Update select-column.js

--- js/select-column.js
+++ js/select-column.js
@@ -1,15 +1,26 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <link rel="stylesheet" href="https://p.trellocdn.com/power-up.min.css">
-    <script src="https://p.trellocdn.com/power-up.min.js"></script>
-</head>
-<body>
-    <div id="content">
-        <h2>Select Column for Focus Mode</h2>
-        <select id="column-select"></select>
-        <button id="start-focus-btn">Start Focus</button>
-    </div>
-    <script src="./js/select-column.js"></script>
-</body>
-</html>
+var t = TrelloPowerUp.iframe();
+
+function fetchColumns() {
+    return t.lists('all').then(function(lists) {
+        var select = document.getElementById('column-select');
+        lists.forEach(function(list) {
+            var option = document.createElement('option');
+            option.value = list.id;
+            option.text = list.name;
+            select.add(option);
+        });
+    });
+}
+
+document.getElementById('start-focus-btn').addEventListener('click', function() {
+    var listId = document.getElementById('column-select').value;
+    t.set('board', 'shared', 'focusListId', listId).then(function() {
+        return t.closePopup();
+    }).then(function() {
+        window.location.href = './focus-mode.html?listId=' + listId;
+    });
+});
+
+t.render(function() {
+    fetchColumns();
+});
